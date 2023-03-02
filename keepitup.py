c581a036a8072371425825d9beaefedb76ccbe3d
htribby <hannahtribby@gmail.com>
2023-03-01 22:16:37 -0500

    drew ball and player

--- keepitup.py
+++ keepitup.py
@@ -9,9 +9,15 @@ black = (0,0,0)
 white = (255, 255, 255)
 
 #create window for game
-window = pygame.display.set_mode((700,700))
+window = pygame.display.set_mode((800,800))
 pygame.display.set_caption('Pong')
 window.fill(black)
+
+#create ball
+ball = pygame.Rect(385, 385, 30, 30)
+
+#create player
+player = pygame.Rect(300, 750, 200, 20)
 
 #create loop for game
 while True:
@@ -19,7 +25,9 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-
+    #draw visuals
+    pygame.draw.ellipse(window, white, ball)
+    pygame.draw.rect(window, white, player)
     #update window
     pygame.display.flip()
     #loop runs 60 times per second
