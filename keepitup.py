f05bc55184c59a8d6ec3912d71f78c6f2784dbe6
htribby <hannahtribby@gmail.com>
2023-03-01 23:30:04 -0500

    placed player and ball movement in functions

--- keepitup.py
+++ keepitup.py
@@ -14,15 +14,38 @@ pygame.display.set_caption('Pong')
 
 #create ball
 ball = pygame.Rect(385, 385, 30, 30)
+ball_speed_x = 5
+ball_speed_y = 5
 
+#logic for moving ball around
+def ball_movement():
+    global ball_speed_x, ball_speed_y
+    #make the ball move
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y 
+
+    #create boundaries for ball and make it "bounce" off walls
+    if ball.top <= 0 or ball.bottom >= 800:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= 800:
+        ball_speed_x *= -1
+
+    #interaction between ball and player
+    if ball.colliderect(player):
+        ball_speed_y *= -1
 
 #create player
 player = pygame.Rect(300, 750, 200, 20)
-
-#speed variables
-ball_speed_x = 5
-ball_speed_y = 5
 player_speed = 0
+
+#logic for moving player around
+def player_movement():
+    #player motion
+    player.x += player_speed
+    if player.right >= 800:
+        player.right = 800
+    if player.left <= 0:
+        player.left = 0
 
 #create loop for game
 while True:
@@ -44,26 +67,9 @@ while True:
             if event.key == pygame.K_LEFT:
                 player_speed += 7
 
-    #make the ball move
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y 
-
-    #create boundaries for ball and make it "bounce" off walls
-    if ball.top <= 0 or ball.bottom >= 800:
-        ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= 800:
-        ball_speed_x *= -1
-
-    #interaction between ball and player
-    if ball.colliderect(player):
-        ball_speed_y *= -1
-
-    #player motion
-    player.x += player_speed
-    if player.right >= 800:
-        player.right = 800
-    if player.left <= 0:
-        player.left = 0
+    ball_movement()
+    player_movement()
+    
     #draw visuals
     window.fill(black)
     pygame.draw.ellipse(window, white, ball)
